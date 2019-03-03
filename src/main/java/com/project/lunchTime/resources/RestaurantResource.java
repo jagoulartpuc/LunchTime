0926795f23b4b0e1238941d53f44d0711bcb5d8c
R.Carvalho <you@example.com>
2019-03-02 22:11:14 -0300

    test

--- src/main/java/com/project/lunchTime/resources/RestaurantResource.java
+++ src/main/java/com/project/lunchTime/resources/RestaurantResource.java
@@ -6,15 +6,18 @@ import com.project.lunchTime.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
-@RestController(value = "/restaurants")
+@RestController
+@RequestMapping("/restaurants")
 public class RestaurantResource {
 
     private RestaurantService service;
@@ -27,6 +30,11 @@ public class RestaurantResource {
     @GetMapping
     public List<Restaurant> findAll() {
         return service.findAll();
+    }
+
+    @GetMapping(value = "/test/{id}")
+    public String test(@PathVariable("id") String txt) {
+        return "test".concat(txt);
     }
 
     @PostMapping
--- src/main/resources/db/migration/V01__create_table_users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE User(
- id              BIGINT(50) PRIMARY KEY AUTO_INCREMENT,
- email                VARCHAR(130) NOT NULL UNIQUE,
- password             VARCHAR(280),
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO user (email,password) VALUES ('admin@admin.com','$2a$10$Y/hdSxxan79Rws1VqyHWk.L5zpC4lnGkYv0DEoFv/KNVgBVEmJsea');
